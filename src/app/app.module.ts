02f8a4765009791a3a8cc5bc057b27ead57128a8
BobbiliMedha <medhabobbili@gmail.com>
2019-03-27 13:50:27 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,28 +3,48 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
+import {IonicStorageModule} from "@ionic/storage";
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-
+import { AddPlacePage } from '../pages/add-place/add-place';
+import { PlacePage } from '../pages/place/place';
+import { SetLocationPage } from '../pages/set-location/set-location';
+import {AgmCoreModule} from '@agm/core';
+import { PlacesService } from '../services/places';
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    AddPlacePage,
+    PlacePage,
+    SetLocationPage
+
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AgmCoreModule.forRoot({
+      apiKey:'AIzaSyCQQ4I4tG2Up4FwO0DRZ1FMVTkH085JI8I' 
+     
+      
+    }),
+    IonicStorageModule.forRoot()
+
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    AddPlacePage,
+    PlacePage,
+    SetLocationPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    PlacesService,
+  
   ]
 })
 export class AppModule {}
